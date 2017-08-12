ba922375f70159c328060cf7b8f5606f0d839e5d
kunal_sharma13 <kunalsharma.ks13@gmail.com>
2017-08-12 20:52:57 +0530

    1st commit

--- app/application/controllers/Api.php
+++ app/application/controllers/Api.php
@@ -18,9 +18,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 		function insert_user(){
 			echo "api call input_user";
-			$username=$this->input->post('username');
+			$username=$this->input->get('username');
 			echo $username;
 			$this->load->model('lisd_model');
+			echo "api call input_user";
 		}
 	}
 
