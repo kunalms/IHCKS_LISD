29676d7f2d4b949a4d74bf1e1b14afd9bd9728df
kunal_sharma13 <kunalsharma.ks13@gmail.com>
2017-08-12 19:00:38 +0530

    model created

--- app/application/controllers/Welcome.php
+++ app/application/controllers/Welcome.php
@@ -20,6 +20,10 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->model('lisd_model');
+	}
+	public function insert_user(){
+
+
 	}
 }
