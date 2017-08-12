bc06e0e167ad38094ba74bf8f54e30704f6b971d
kunal_sharma13 <kunalsharma.ks13@gmail.com>
2017-08-12 22:23:36 +0530

    htaccess done

--- app/application/controllers/Api.php
+++ app/application/controllers/Api.php
@@ -25,16 +25,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			$data['user_name']=$username;
 			$data['user_password']=$password;
 			$res=$this->lisd_model->validate_user($data);
-			print_r($res);
+
 			if(sizeof($res)>0)
 			{
-			$data['sessionId']=$res['user_id'];
-			$data['message']="login successful";
-			echo json_encode($data);
+			$ret['sessionId']=$res['user_id'];
+			$ret['message']="login successful";
+			echo json_encode($ret);
 			}
 			else{
-			$data['message']="login successful";
-			echo json_encode($data);	
+			$ret['message']="login successful";
+			echo json_encode($ret);	
 			}
 
 		}
