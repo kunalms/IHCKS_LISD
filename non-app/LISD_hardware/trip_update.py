9ef0a304f4f2facfcc6e4cd2f0c9d2c786ca1f7e
Sohail Chamadia <sohailyc@gmail.com>
2017-08-13 04:52:12 +0530

    API MOD

--- non-app/LISD_hardware/trip_update.py
+++ non-app/LISD_hardware/trip_update.py
@@ -10,26 +10,27 @@ def trip_cont(start_time,flag,tripid):
 		location=loc()
 		dt=datetime.now()
 		cur_time=int(time.mktime(dt.timetuple()))
-		latitude=(location[0])
-		longitude=(location[1])
-		if latitude != "0.0" and longitude != "0.0":
-			userdata={}
-			userdata["trip_id"]=tripid
-			userdata["user_id"]="109"
-			userdata["vehicle_id"]="104"
-			userdata["timestamp"]=cur_time
-			userdata["latitude"]=latitude
-			userdata["longitude"]=longitude
-			userdata["is_trip_live"]=flag;
-			link = """https://api.expertise97.hasura-app.io/api/insert_trip?trip_id={str0}&user_id={str1}&vehicle_id={str2}&latitude={str3}&longitude={str4}&timestamp={str5}&is_trip_live={str6}""".format(str0=userdata["trip_id"],
-																									str1=userdata["user_id"],
-																									str2=userdata["vehicle_id"],
-																									str3=userdata["latitude"],
-																									str4=userdata["longitude"],
-																									str5=userdata["timestamp"],
-																									str6=userdata["is_trip_live"])
-			print(link)
-			resp = requests.get(link,timeout=10)
-			comp=True
-	if flag:
-		return cur_time-start_time;
+		if location:
+			latitude=(location[0])
+			longitude=(location[1])
+			if latitude != "0.0" and longitude != "0.0":
+				userdata={}
+				userdata["trip_id"]=tripid
+				userdata["user_id"]="109"
+				userdata["vehicle_id"]="104"
+				userdata["timestamp"]=dt
+				userdata["latitude"]=latitude
+				userdata["longitude"]=longitude
+				userdata["is_trip_live"]=flag;
+				link = """https://api.expertise97.hasura-app.io/api/insert_trip?trip_id={str0}&user_id={str1}&vehicle_id={str2}&latitude={str3}&longitude={str4}&timestamp={str5}&is_trip_live={str6}""".format(str0=userdata["trip_id"],
+																										str1=userdata["user_id"],
+																										str2=userdata["vehicle_id"],
+																										str3=userdata["latitude"],
+																										str4=userdata["longitude"],
+																										str5=userdata["timestamp"],
+																										str6=userdata["is_trip_live"])
+				print(link)
+				resp = requests.get(link,timeout=10)
+				comp=True
+		if flag:
+			return cur_time-start_time;
