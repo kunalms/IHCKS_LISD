a552235ac639d5d18433b06dfd787c7ce4901193
kunal_sharma13 <kunalsharma.ks13@gmail.com>
2017-08-25 22:42:48 +0530

    project files updated

--- app/application/config/config.php
+++ app/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://api.expertise97.hasura-app.io/';
+$config['base_url'] = 'https://lisd.backslapping80.hasura-app.io/';
 
 /*
 |--------------------------------------------------------------------------
