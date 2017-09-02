fc0d015c379f2e0310dfb219147de57a545cb9f8
kunal_sharma13 <kunalsharma.ks13@gmail.com>
2017-09-03 02:33:40 +0530

    gps testing

--- app/application/models/Lisd_model.php
+++ app/application/models/Lisd_model.php
@@ -96,7 +96,7 @@
 
 		function fetch_gps_user($data)
 		{
-			$this->db->group_by("title");
+			$this->db->group_by("trip_id");
 			$this->db->from('trip_details');
 			$this->db->where($data);
 			$query=$this->db->get();
